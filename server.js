7a083c7eb4fb3be343e140c65e1a3369feaf470c
Usman <uliaqut@wilbrad.com.au>
2023-10-16 15:17:31 +1100

    Changes to the cors api to enforce Https protocol is used

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
 const express = require('express');
 const corsAnywhere = require('cors-anywhere');
-const cors = require('cors');
 const app = express();
 
 const server = corsAnywhere.createServer({
   originWhitelist: [], // Set this to your desired whitelist of allowed origins
   requireHeader: [],
   removeHeaders: [],
-  compress: false, // Disable content compression
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: [
+    'cookie',
+    'cookie2',
+    // Strip Heroku-specific headers
+    'x-request-start',
+    'x-request-id',
+    'via',
+    'connect-time',
+    'total-route-time',
+    // Other Heroku added debug headers
+    // 'x-forwarded-for',
+    // 'x-forwarded-proto',
+    // 'x-forwarded-port',
+  ],
+  redirectSameOrigin: true,
+  httpProxyOptions: {
+    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+    xfwd: false,
+  },
 });
-app.use(cors());
 app.use((req, res, next) => {
+  //req.headers['x-forwarded-proto'] = 'https';
   server.emit('request', req, res, next);
 });
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
   if ('OPTIONS' == req.method) {
   res.sendStatus(200);
   } else {
     next();
   }
 });
-const port = 8080; // Specify the desired port number
+const port = process.env.PORT || 8080; // Specify the desired port number
 
 app.listen(port, () => {
  console.log(`Local development server started on port ${port}`);
